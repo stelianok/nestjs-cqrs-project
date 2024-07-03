2d9ed42f2efb810dc63370954424cb9db48aca56
Kauã Steliano <stelianok@gmail.com>
2024-07-03 15:33:51 -0300

    Implemented command to assign a new manager to employee

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -6,6 +6,8 @@ import { CreateEmployeeDto } from './commands/create-employee/create-employee.dt
 import { CreateEmployeeCommand } from './commands/create-employee/create-employee.command';
 import { UpdateEmployeeDto } from './commands/update-employee/update-employee.dto';
 import { UpdateEmployeeCommand } from './commands/update-employee/update-employee.command';
+import { AssignManagerCommand } from './commands/assign-manager/assign-manager-command';
+import { AssignManagerDto } from './commands/assign-manager/assign-manager.dto';
 
 @Controller('employees')
 export class EmployeesController {
@@ -37,6 +39,22 @@ export class EmployeesController {
   @Patch(':id')
   async update(@Param('id') id: string, @Body() dto: UpdateEmployeeDto) {
     const command = plainToClass(UpdateEmployeeCommand, {
+      ...dto,
+      id: Number(id),
+    });
+
+    const affectedRows = await this.commandBus.execute(command);
+
+    if (!affectedRows) throw new NotFoundException();
+
+    const query = plainToClass(GetEmployeeQuery, { id });
+
+    return this.queryBus.execute(query);
+  }
+
+  @Patch(':id/assign-manager')
+  async assignManager(@Param('id') id: string, @Body() dto: AssignManagerDto) {
+    const command = plainToClass(AssignManagerCommand, {
       ...dto,
       id: Number(id),
     });
